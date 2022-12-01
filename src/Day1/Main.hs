6e33ae28999cd2f6e1b05cdb7eef1cdfb2892316
Simre1 <simre4775@gmail.com>
2022-12-01 09:01:23 +0100

    Day1 added

--- src/Day1/Main.hs
+++ src/Day1/Main.hs
@@ -1,7 +1,24 @@
 module Day1.Main where
 
+import Data.List.Split
+import Debug.Trace
+import Data.List (sortBy)
+
+
 solution1 :: IO ()
-solution1 = pure ()
+solution1 = readInput >>= print . maxFood . parseInput
 
 solution2 :: IO ()
-solution2 = pure ()
+solution2 = readInput >>= print . totalFoodForTop3 . parseInput
+
+readInput :: IO String
+readInput = readFile "inputs/day1/input1"
+
+parseInput :: String -> [[Int]]
+parseInput = fmap (fmap read)  . split (dropDelims $ oneOf [""])  . lines
+
+maxFood :: [[Int]] -> Int
+maxFood = maximum . fmap sum
+
+totalFoodForTop3 :: [[Int]] -> Int
+totalFoodForTop3 = sum . take 3 . sortBy (flip compare) . fmap sum
