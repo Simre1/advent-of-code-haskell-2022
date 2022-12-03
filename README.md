7aaa7a513a6ce39d2c770fca49483efe9bdbcc22
Simre1 <simre4775@gmail.com>
2022-12-03 12:29:36 +0100

    Day 3 added

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ My solutions to the coding problems in Advent of Code 2022.
 
 - [x] Day 1
 - [x] Day 2
-- [ ] Day 3
+- [x] Day 3
 - [ ] Day 4
 - [ ] Day 5
 - [ ] Day 6
--- src/Day3/Main.hs
+++ src/Day3/Main.hs
@@ -1,7 +1,45 @@
 module Day3.Main where
 
+import Data.Functor
+import Data.List.Split
+import Data.Map qualified as M
+import Data.Set qualified as S
+
+data Rucksack = Rucksack (S.Set Char) (S.Set Char) deriving (Show, Eq)
+
+input :: IO [Rucksack]
+input = do
+  file <- readFile "inputs/day3/test"
+  pure $
+    lines file <&> \line ->
+      let splitIndex = (length line `quot` 2)
+          firstHalf = take splitIndex line
+          secondHalf = drop splitIndex line
+       in Rucksack (S.fromList firstHalf) (S.fromList secondHalf)
+
 solution1 :: IO ()
-solution1 = pure ()
+solution1 = do
+  rucksacks <- input
+  print $ sum $ getPriority . commonItem <$> rucksacks
 
 solution2 :: IO ()
-solution2 = pure ()
+solution2 = do
+  rucksacks <- input
+  let groups = chunksOf 3 rucksacks
+      badges = groups <&> \[r1, r2, r3] -> commonBadge r1 r2 r3
+  print $ sum $ getPriority <$> badges
+
+commonBadge :: Rucksack -> Rucksack -> Rucksack -> Char
+commonBadge r1 r2 r3 = head $ S.toList $ getContent r1 `S.intersection` getContent r2 `S.intersection` getContent r3
+
+commonItem :: Rucksack -> Char
+commonItem (Rucksack first second) = head $ S.toList $ S.intersection first second
+
+getPriority :: Char -> Int
+getPriority c = priorityMap M.! c
+  where
+    priorityMap :: M.Map Char Int
+    priorityMap = M.fromList $ (['a' .. 'z'] ++ ['A' .. 'Z']) `zip` [1 .. 52]
+
+getContent :: Rucksack -> S.Set Char
+getContent (Rucksack first second) = S.union first second
